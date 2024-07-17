64f4ffe9c007377679f0ec50d3071bc7e54e2119
Vigas <vigasdeep@gmail.com>
2024-07-17 17:55:54 +0530

    add test for /ping

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestPingEndpoint tests the /ping endpoint
+func TestPingEndpoint(t *testing.T) {
+  router := SetupRouter()
+
+  w := httptest.NewRecorder()
+  req, _ := http.NewRequest("GET", "/ping", nil)
+  router.ServeHTTP(w, req)
+
+  assert.Equal(t, http.StatusOK, w.Code)
+  assert.JSONEq(t, `{"message": "pong"}`, w.Body.String())
+}
