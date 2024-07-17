20934a7cf9bc9759747e187a08fa3d9c2395f18b
Vigas <vigasdeep@gmail.com>
2024-07-17 17:55:34 +0530

    separate the router

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
 
 func main() {
-  r := gin.Default()
-
-  // Define a GET endpoint
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
+  r := SetupRouter()
 
   // Start the server
   r.Run(":8080") // listen and serve on 0.0.0.0:8080
---
+++ router.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+  
+	// Define a GET endpoint
+	r.GET("/ping", func(c *gin.Context) {
+	  c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	  })
+	})
+  
+	// // Define product routes
+	// r.GET("/products", getProducts)
+	// r.GET("/products/:id", getProduct)
+	// r.POST("/products", createProduct)
+	// r.PUT("/products/:id", updateProduct)
+	// r.DELETE("/products/:id", deleteProduct)
+  
+	return r
+}
