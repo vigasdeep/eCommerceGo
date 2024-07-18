f20248466feefd9d54228337ee9ae5e15d1b65d7
Vigas <vigasdeep@gmail.com>
2024-07-18 16:57:51 +0530

    fixed #1 adds unit tests for products

--- tests/product_test.go
+++ tests/product_test.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+	"strings"
 	// "github.com/gin-gonic/gin"
 
 	"github.com/stretchr/testify/assert"
@@ -20,45 +20,51 @@ func TestGetProducts(t *testing.T) {
     w := httptest.NewRecorder()
     req, _ := http.NewRequest("GET", "/products", nil)
     r.ServeHTTP(w, req)
+    // fmt.Printf("Response: %s\n", w.Body.String()) // Log the response body
 
     assert.Equal(t, http.StatusOK, w.Code)
-    assert.JSONEq(t, `{"message": "Get all products"}`, w.Body.String())
 }
 
-// func TestGetProduct(t *testing.T) {
-//     r := router.SetupRouter()
-//     w := httptest.NewRecorder()
-//     req, _ := http.NewRequest("GET", "/products/1", nil)
-//     r.ServeHTTP(w, req)
+func TestGetProduct(t *testing.T) {
+    r := router.SetupRouter()
+    w := httptest.NewRecorder()
+    req, _ := http.NewRequest("GET", "/products/1", nil)
+    r.ServeHTTP(w, req)
 	
-//     assert.Equal(t, http.StatusOK, w.Code)
-//     assert.JSONEq(t, `{"message": "Get a single product"}`, w.Body.String())
-// }
+    assert.Equal(t, http.StatusOK, w.Code)
+    assert.Regexp(t, `{"id":[0-9]+,"name":"Product [A-Za-z0-9]+","price":[0-9]+}`, w.Body.String())
+}
 
-// func TestCreateProduct(t *testing.T) {
-//     r := router.SetupRouter()
-//     w := httptest.NewRecorder()
-//     req, _ := http.NewRequest("POST", "/products", nil)
-//     r.ServeHTTP(w, req)
+func TestCreateProduct(t *testing.T) {
+    r := router.SetupRouter()
+    w := httptest.NewRecorder()
+    productJSON := `{"name": "Random Product", "price": 123}`
+    req, _ := http.NewRequest("POST", "/products", strings.NewReader(productJSON))
+    req.Header.Set("Content-Type", "application/json")
+    r.ServeHTTP(w, req)
 
-//     assert.Equal(t, http.StatusOK, w.Code)
-//     assert.JSONEq(t, `{"message": "Create a new product"}`, w.Body.String())
-// }
+    assert.Equal(t, http.StatusCreated, w.Code)
+    // assert.Regexp(t, `{"id": [0-9]+, "name": "Random Product", "price": 123}`, w.Body.String())
+}
 
-// func TestUpdateProduct(t *testing.T) {
-//     r := router.SetupRouter()
-//     w := httptest.NewRecorder()
-//     req, _ := http.NewRequest("PUT", "/products/1", nil)
-//     r.ServeHTTP(w, req)
-// 	fmt.Printf("%+v\n", w)
-//     assert.Equal(t, http.StatusOK, w.Code)
-//     assert.JSONEq(t, `{"message": "Update a product"}`, w.Body.String())
-// }
+func TestUpdateProduct(t *testing.T) {
+    r := router.SetupRouter()
+    w := httptest.NewRecorder()
+	productJSON := `{"name": "Random Product", "price": 123}`
+
+    req, _ := http.NewRequest("PUT", "/products/1", strings.NewReader(productJSON))
+	req.Header.Set("Content-Type", "application/json")
+    r.ServeHTTP(w, req)
+
+	// fmt.Printf("%+v\n", w)
+    assert.Equal(t, http.StatusOK, w.Code)
+    // assert.JSONEq(t, `{"message": "Update a product"}`, w.Body.String())
+}
 
 func TestDeleteProduct(t *testing.T) {
     r := router.SetupRouter()
     w := httptest.NewRecorder()
-    req, _ := http.NewRequest("DELETE", "/products/1", nil)
+    req, _ := http.NewRequest("DELETE", "/products/2", nil)
     r.ServeHTTP(w, req)
 
     assert.Equal(t, http.StatusOK, w.Code)
